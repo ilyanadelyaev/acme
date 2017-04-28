c622e3e350d7cd33ed53ef39f9e2d0cf95f05ec2
i.nadelyaev <i.nadelyaev@rambler-co.ru>
2017-04-28 16:32:08 +0300

    HEROKU init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn acme.neuro.api.run:app.flask_app --worker-class gevent -w 2
---
+++ acme/neuro/api/run.py
@@ -0,0 +1,8 @@
+import acme.neuro.api.application as _application
+
+
+app = _application.Application()
+
+
+if __name__ == '__main__':
+    app.run()
